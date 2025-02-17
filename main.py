c269bce8f2508fff04a8c979551368aa79994e01
barasa <101450156+barasapeter@users.noreply.github.com>
2025-02-17 10:51:15 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# The code imports several modules, including shelve, pygame, os, sys, subprocess, and tkinter.
+# The code imports several modules, including shelve, pygame, os, sys, subprocess, and 
 # It also defines a list of database files and three functions: record_user_data(), view_scores(), and StartGame
 
 # The record_user_data() function stores user data in a database file named ".gamedata" using the shelve module
